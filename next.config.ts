2c4b044f52bee54da3fab6836ce14e52686b4086
Skip McCormick <kinnicoun@gmail.com>
2026-01-15 19:27:58 +0100

    config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
+const repo = "portfolio";
+
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   images: { unoptimized: true },
-  // basePath: "/portfolio",
-  // assetPrefix: "/portfolio/",
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 };
 
 export default nextConfig;
