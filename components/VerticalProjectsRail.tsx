44d956bcb6133b30869a4c80506610440c07c333
Skip McCormick <kinnicoun@gmail.com>
2026-03-23 18:27:18 +0100

    ...

--- components/VerticalProjectsRail.tsx
+++ components/VerticalProjectsRail.tsx
@@ -436,6 +436,7 @@ export default function VerticalProjectsRail({ projects }: { projects: VerticalP
                                                 <a
                                                     className="inline-flex items-center justify-center rounded-xl border border-white/15 bg-white/5 px-4 py-2 text-sm font-medium text-white hover:bg-white/10"
                                                     href={p.linkProject}
+                                                    target="_blank"
                                                 >
                                                     View project
                                                 </a>
